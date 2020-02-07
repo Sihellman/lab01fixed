0743e7dff131edcf3a5a2321312162929f9cc5ec
Sivia <sivia.hellman@ti.htc.edu>
2020-02-06 20:55:15 -0600

    added print

--- src/main/java/edu/htc/blitstein/HelloWorld2.java
+++ src/main/java/edu/htc/blitstein/HelloWorld2.java
@@ -3,6 +3,8 @@ package edu.htc.blitstein;
 public class HelloWorld2 {
     public static void main(String...args){
        System.out.println("hi");
+       System.out.println("hi");
        System.exit(45);
+
     }
 }
